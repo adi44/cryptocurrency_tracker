f58a189b4e44a8e6c46f9bf1279a4acccfe52c50
root <adityadhir97@gmail.com>
2020-06-28 22:41:14 +0530

    Updates

---
+++ precache-manifest.eef66cf05eaa160b7c46ef2de9956956.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1e761ce1e492589266771ec75c4a43",
+    "url": "/Cryptocurrency-tracker/index.html"
+  },
+  {
+    "revision": "844f25402c9ff45e0d2a",
+    "url": "/Cryptocurrency-tracker/static/css/main.b9eae31c.chunk.css"
+  },
+  {
+    "revision": "2c77517d88c483822b5a",
+    "url": "/Cryptocurrency-tracker/static/js/2.3c47ef3a.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/Cryptocurrency-tracker/static/js/2.3c47ef3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "844f25402c9ff45e0d2a",
+    "url": "/Cryptocurrency-tracker/static/js/main.06857e6f.chunk.js"
+  },
+  {
+    "revision": "59a973c0c5c502215039",
+    "url": "/Cryptocurrency-tracker/static/js/runtime-main.c89157f7.js"
+  },
+  {
+    "revision": "cd62a9ab137da920ed9a0a268e9b1846",
+    "url": "/Cryptocurrency-tracker/static/media/BCH.cd62a9ab.jpg"
+  },
+  {
+    "revision": "290012f692c57ad3071957b080f285dc",
+    "url": "/Cryptocurrency-tracker/static/media/ETH.290012f6.jpg"
+  },
+  {
+    "revision": "c4d6b6a5c1b8b3c7cf588db6e71a17e5",
+    "url": "/Cryptocurrency-tracker/static/media/TRX.c4d6b6a5.jpg"
+  },
+  {
+    "revision": "8eb0945f5e785472d2b68aa09b233146",
+    "url": "/Cryptocurrency-tracker/static/media/XLM.8eb0945f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cryptocurrency-tracker/precache-manifest.65411174bfba13a53bcf36d0fd90e52d.js"
+  "/Cryptocurrency-tracker/precache-manifest.eef66cf05eaa160b7c46ef2de9956956.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cryptocurrency-tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Cryptocurrency-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
