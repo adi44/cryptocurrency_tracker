0fc6f65c8bf02cff30dd443f710aa5915c7962aa
root <adityadhir97@gmail.com>
2020-06-28 21:50:59 +0530

    Updates

---
+++ precache-manifest.73cc29d1514a11dc2c3463af8188ec20.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5359bc77c9732905495dc871478dce",
+    "url": "/cryptocurrency-tracker/index.html"
+  },
+  {
+    "revision": "8fdaeb8e98c0a7970570",
+    "url": "/cryptocurrency-tracker/static/css/main.b9eae31c.chunk.css"
+  },
+  {
+    "revision": "5f675b52d53f41379812",
+    "url": "/cryptocurrency-tracker/static/js/2.a661b195.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/cryptocurrency-tracker/static/js/2.a661b195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fdaeb8e98c0a7970570",
+    "url": "/cryptocurrency-tracker/static/js/main.6505ad30.chunk.js"
+  },
+  {
+    "revision": "b20d06a195ce130e10c2",
+    "url": "/cryptocurrency-tracker/static/js/runtime-main.4b17170a.js"
+  },
+  {
+    "revision": "cd62a9ab137da920ed9a0a268e9b1846",
+    "url": "/cryptocurrency-tracker/static/media/BCH.cd62a9ab.jpg"
+  },
+  {
+    "revision": "290012f692c57ad3071957b080f285dc",
+    "url": "/cryptocurrency-tracker/static/media/ETH.290012f6.jpg"
+  },
+  {
+    "revision": "c4d6b6a5c1b8b3c7cf588db6e71a17e5",
+    "url": "/cryptocurrency-tracker/static/media/TRX.c4d6b6a5.jpg"
+  },
+  {
+    "revision": "8eb0945f5e785472d2b68aa09b233146",
+    "url": "/cryptocurrency-tracker/static/media/XLM.8eb0945f.jpg"
+  }
+]);
